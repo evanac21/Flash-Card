335732fd349e5214905798c2fc9aacba521cd419
evanac21 <evanac21@gmail.com>
2018-11-07 22:39:19 -0800

    Code Updates for 10:38PM on 11/7/18
    Added settings form and began implementing term and defeniton stuff

--- src/flash/cards/FlashCards.java
+++ src/flash/cards/FlashCards.java
@@ -4,16 +4,29 @@ import javax.swing.JFrame;
 
     public class FlashCards {
          static flashPanel fp = new flashPanel();
+         static settingForm sf = new settingForm();
     public static void main(String[] args) {
-           
             JFrame f = new JFrame("Flash Cards");
             f.add(fp);
             f.setVisible(true);
             f.setSize(600, 300);
             fp.startButton.addActionListener(e-> start());
+            fp.settings.addActionListener(e-> settings());
     }private static void start() {
-        fp.responseField.setText("");
-        fp.wordField.setText("");
+       respo("");
+       wf("");
+      
     }
-    
+    private static void respo(String print) {
+        fp.responseField.setText(print);
+    }
+    private static void wf(String print) {
+        fp.wordField.setText(print);
+    }
+    private static void settings() {
+        JFrame frame = new JFrame("Settings");
+        frame.add(sf);
+        frame.setVisible(true);
+        frame.setSize(600, 300);
+    }
 }
